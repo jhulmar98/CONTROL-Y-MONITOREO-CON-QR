8df8e4a33a8b60ddfce412c301bf7c368730b53f
jhulmar98 <jhulmarsoftware@gmail.com>
2025-10-27 08:35:15 -0500

    locales.js

--- js/locales.js
+++ js/locales.js
@@ -34,5 +34,51 @@ export const LOCALES = {
     lng: -77.026381
   },
 
+  "L005-4.2": {
+    id: "L005-4.2",
+    label: "Edificio Torre Begonias",
+    direccion: "CALLE LAS BEGONIAS 415",
+    lat: -12.092493,
+    lng: -77.023986
+  },
+  "L006-4.2": {
+    id: "L006-4.2",
+    label: "Edificio Plaza del Sol",
+    direccion: "CALLE LAS BEGONIAS 441",
+    lat: -12.092924,
+    lng: -77.024163
+  },
+  "L007-4.2": {
+    id: "L007-4.2",
+    label: "Edificio Torre Parque 1",
+    direccion: "CALLE LAS BEGONIAS 526",
+    lat: -12.093526,
+    lng: -77.024950
+  },
+   "L008-4.2": {
+    id: "L008-4.2",
+    label: "Edificio Torre Parque 2",
+    direccion: "CALLE LAS BEGONIAS 620",
+    lat: -12.094380,
+    lng: -77.025420
+  },
+   "L009-4.2": {
+    id: "L009-4.2",
+    label: "Edificio el Consorcio",
+    direccion: "CALLE CHINCHON 1018",
+    lat: -12.095134,
+    lng: -77.026039
+  },
+    "L010-4.4": {
+    id: "L010-4.4",
+    label: "Edificio Alto Caral",
+    direccion: "AV. DIONISIO DERTEANO 150",
+    lat: -12.095586,
+    lng: -77.021884
+  },
 
+
+  
 };
+
+
